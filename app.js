a51e367a64651ccc9063b109b017e90fa7bb83db
IronIdk <jmsfo@iscte-iul.pt>
2023-03-27 16:38:22 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const btnP1 = document.getElementById('btn-p1');
+btnP1.addEventListener('click', () => {
+  window.location.href = 'part1/part1.html';
+});
+
+
+const btnP2 = document.getElementById('btn-p2');
+btnP2.addEventListener('click', () => {
+  window.location.href = 'part2/part2.html';
+});

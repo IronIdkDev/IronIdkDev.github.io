18d5733b4f56aa5dbc2a3a7eff624fbcaee4f32c
IronIdk <jmsfo@iscte-iul.pt>
2023-06-13 11:22:40 +0100

    Disabled Part 1

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const btnP1 = document.getElementById('btn-p1');
+/*const btnP1 = document.getElementById('btn-p1');
 btnP1.addEventListener('click', () => {
   window.location.href = 'part1/part1.html';
+});*/
+const btnP1 = document.getElementById('btn-p1');
+btnP1.addEventListener('click', () => {
+  alert('Part 1 is currently disabled');
 });
-
 
 const btnP2 = document.getElementById('btn-p2');
 btnP2.addEventListener('click', () => {
   window.location.href = 'part2/part2.html';
 });
-
 
 const btnQuiz = document.querySelector('.practice-quiz-button');
 btnQuiz.addEventListener('click', () => {

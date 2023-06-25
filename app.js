d64c2411c5268e4292bf01e647b569765cc46452
IronIdk <jmsfo@iscte-iul.pt>
2023-06-25 01:52:30 +0100

    Started developing my website for personal projects

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-/*const btnP1 = document.getElementById('btn-p1');
-btnP1.addEventListener('click', () => {
-  window.location.href = 'part1/part1.html';
-});*/
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Disable Part 1 button
 const btnP1 = document.getElementById('btn-p1');
 btnP1.addEventListener('click', () => {
   alert('Part 1 is currently disabled');
 });
 
+// Redirect to Part 2
 const btnP2 = document.getElementById('btn-p2');
 btnP2.addEventListener('click', () => {
   window.location.href = 'part2/part2.html';
 });
 
-const btnQuiz = document.querySelector('.practice-quiz-button');
-btnQuiz.addEventListener('click', () => {
-  window.location.href = 'https://take.quiz-maker.com/QPCARQYUW';
-});
+// Toggle menu
+function toggleMenu() {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.nav');
+
+  hamburger.classList.toggle('open');
+  nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+}

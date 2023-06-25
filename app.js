025d06c5b266fe920c24d7026691433ecd7ff36d
IronIdk <jmsfo@iscte-iul.pt>
2023-06-25 19:45:02 +0100

    Added toggle dark/light theme, but no icons

--- app.js
+++ app.js
@@ -24,3 +24,20 @@ function toggleMenu() {
     nav.style.transform = 'translateY(-100%)';
   }
 }
+
+// Theme toggle functionality
+const themeToggleCheckbox = document.querySelector('.theme-toggle-checkbox');
+const body = document.querySelector('body');
+
+// Set default theme to dark
+body.classList.add('dark-theme');
+
+themeToggleCheckbox.addEventListener('change', function () {
+  if (this.checked) {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+  } else {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+  }
+});
